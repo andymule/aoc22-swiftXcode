3e3cf533198718d5f9621f9b6a81ee18363a9474
Andy Muehlhausen <mule@apple.com>
2022-12-12 07:31:31 -0800

    day12WIP

--- AOC22-xcode/ContentView.swift
+++ AOC22-xcode/ContentView.swift
@@ -1,21 +1,127 @@
-//import SwiftGraph
+import SwiftGraph
 import SwiftUI
-//import TextView
-//import simd
+import TextView
+import simd
 //import VectorKit
 //import Accelerate
 //import BigNumber
 
 /// this command will prInt64 to terminal the output of the preview screen
 /// & should run in or your local active folder
-/// ~/Library/Developer/Xcode/UserData/Previews/Simulator\ Devices/53F099E6-21B0-4FE4-94CF-7BA756439400/data/Containers/Data/Application
+/// ~/Library/Developer/Xcode/UserData/Previews/Simulator Devices/D838DED4-7024-4819-9044-10E54C7733AE/data/Containers/Data
 /// watch -n 0.1 find . -name "AOC.txt" -exec cat {} +
 /// watch -n 0.1 pkill "Problem Reporter"
 /// 􀆔+􀆕+P reloads preview if stopped
 class Mine {
     func run() {
+        class Point: Codable, Equatable, Hashable {
+            static func == (lhs: Point, rhs: Point) -> Bool {
+                return lhs.pointS == rhs.pointS
+            }
+            let point: [Int]
+            let pointS: String
+            var h: Int
+            var graphI: Int = -1
+            var startPoint = false
+            var endPoint = false
+            init(_ h: Character, _ p: [Int]) {
+                self.h = Int(h.asciiValue!) - 97
+                point = p
+                pointS = String(p[0]) + "," + String(p[1])
+            }
+            func near(_ nextp: Point) -> Bool {
+                let nextpx = nextp.h
+                return nextpx == h || nextpx == h + 1 // nextpx == h - 1 //||
+            }
+            func hash(into hasher: inout Hasher) {
+                hasher.combine(point[0] * 100)
+                hasher.combine(point[1])
+            }
+        }
+
+        var graph: WeightedGraph<String, Int> = .init()
+//        var graph: Graph<[Point], Int> = .init()
+//        graph.vertices.insert(contentsOf: <#T##C#>, at: <#T##Self.Index#>)
         loadInput("day12")
-        pr(input)
+//        pr(input)
+        var map: [[Int]: Point] = .init()
+        let lines = input.components(separatedBy: "\n")
+        for y in 0..<lines.count {
+            let line = lines[y]
+            for x in 0..<line.count {
+                let newP = Point(line[x], [y, x])
+                if line[x] == "S" {
+                    newP.h = 0
+                    newP.startPoint = true
+                }
+                if line[x] == "E" {
+                    newP.h = 25
+                    newP.endPoint = true
+                }
+                map[[y, x]] = newP
+                newP.graphI = graph.addVertex(newP.pointS)
+                pr(newP.pointS,newP.point,newP.graphI,newP.h,newP.startPoint || newP.endPoint)
+//                pr(newP.pointS)
+//                pr(xxx)
+            }
+        }
+//        var startI = -1
+//        var endI = -1
+        for y in 0..<lines.count {
+            let line = lines[y]
+            for x in 0..<line.count {
+                let thisP = map[[y, x]]!
+                if y > 1 {
+                    let thatP = map[[y - 1, x]]!
+                    if thisP.near(thatP) {
+
+                        graph.addEdge(from: thisP.pointS, to: thatP.pointS, weight: thatP.h - thisP.h)
+//                        graph.add
+                    }
+                }
+                if y < lines.count - 1 {
+                    let thatP = map[[y + 1, x]]!
+                    if thisP.near(thatP) {
+                        graph.addEdge(from: thisP.pointS, to: thatP.pointS, weight: thatP.h - thisP.h)
+                    }
+                }
+                if x > 1 {
+                    let thatP = map[[y, x - 1]]!
+                    if thisP.near(thatP) {
+                        graph.addEdge(from: thisP.pointS, to: thatP.pointS, weight: thatP.h - thisP.h)
+                    }
+                }
+                if x < line.count - 1 {
+                    let thatP = map[[y, x + 1]]!
+                    if thisP.near(thatP) {
+                        graph.addEdge(from: thisP.pointS, to: thatP.pointS, weight: thatP.h - thisP.h)
+                    }
+                }
+            }
+        }
+//        pr(graph.edgeCount)
+//        pr(graph.vertices)
+
+//        pr(map.map({$0.value.h}))
+//        pr(map[[0,0]]?.h)
+        let startP: Point = map.values.filter({ $0.startPoint })[0]
+        let endP: Point = map.values.filter({ $0.endPoint })[0]
+//        for g in graph.ed {
+//            pr(g)
+//        }
+
+//        let (distances, pathDict) = graph.dijkstra(root: startP.pointS, startDistance: 0)
+//        var a = graph.indexOfVertex(startP.pointS)!
+//        var b = graph.indexOfVertex(endP.pointS)!
+//        pr(a, b)
+//        var nameDistance: [String: Int?] = distanceArrayToVertexDict(distances: distances, graph: graph)
+//        // shortest distance from New York to San Francisco
+//        let temp = nameDistance[endP.pointS]
+//        // path between New York and San Francisco
+//        let path: [WeightedEdge<Int>] = pathDictToPath(from: graph.indexOfVertex(startP.pointS)!, to: graph.indexOfVertex(startP.pointS)!, pathDict: pathDict)
+//        let stops: [String] = graph.edgesToVertices(edges: path)
+//        pr(stops)
+
     }
     public var str = ""
     public var input = "NO FILE LOADED"
